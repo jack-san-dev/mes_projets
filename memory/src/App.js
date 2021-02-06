bddecb459754320c1f6784e8d26122e7b6fdd869
MaggCk <axel.maggiochi@epitech.eu>
2021-02-06 14:46:19 +0100

    add events

---
+++ memory/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from './Card'
+import GuessCount from './GuessCount'
+
+class App extends React.Component {
+  handleCardClick(card) {
+    console.log(card, 'clicked')
+  }
+  render() {
+    return (
+      <div className="memory">
+        <GuessCount guesses={0} />
+        <Card card="😀" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
+        <Card card="💖" feedback="justMismatched" onClick={this.handleCardClick} />
+        <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
+        <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
+      </div>
+    )
+  }
+}
+export default App;
