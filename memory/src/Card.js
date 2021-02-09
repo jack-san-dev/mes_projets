6064f636fbe1f4f73e3493861d3ebdb4cfcc4766
MaggCk <axel.maggiochi@epitech.eu>
2021-02-09 15:58:58 +0100

    setState skillz

--- memory/src/Card.js
+++ memory/src/Card.js
@@ -6,8 +6,8 @@ import './Card.css'
 
 const HIDDEN_SYMBOL = '❓'
 
-const Card = ({ card, feedback, onClick }) => (
-    <div className={`card ${feedback}`} onClick={() => onClick(card)}>
+const Card = ({ card, feedback, index, onClick }) => (
+    <div className={`card ${feedback}`} onClick={() => onClick(index)}>
       <span className="symbol">
         {feedback === 'hidden' ? HIDDEN_SYMBOL : card}
       </span>
